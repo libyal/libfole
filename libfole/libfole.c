9b81e2146a7f920723e837fa69bea353c2b4bbe1
Joachim Metz <joachim.metz@gmail.com>
2024-01-19 05:10:31 +0100

    Applied updates

--- libfole/libfole.c
+++ libfole/libfole.c
@@ -1,7 +1,7 @@
 /*
  * Library to support the Object Linking and Embedding (OLE) data types
  *
- * Copyright (C) 2008-2023, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2008-2024, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *

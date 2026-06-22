2603964f5413cb4abc291288c38e01c58f927ad7
Joachim Metz <joachim.metz@gmail.com>
2026-06-22 12:00:38 +0200

    Worked on tests and changes for CI tests

--- include/libfole/error.h
+++ include/libfole/error.h
@@ -82,7 +82,7 @@ enum LIBFOLE_ARGUMENT_ERROR
 	 */
 	LIBFOLE_ARGUMENT_ERROR_UNSUPPORTED_VALUE	= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBFOLE_ARGUMENT_ERROR_CONFLICTING_VALUE	= 9
 };
@@ -214,7 +214,7 @@ enum LIBFOLE_OUTPUT_ERROR
 {
 	LIBFOLE_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBFOLE_OUTPUT_ERROR_INSUFFICIENT_SPACE		= 1
 };

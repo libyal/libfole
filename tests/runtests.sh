e7bf6b6f8bfcee0d0602bc46e2cf2a3e4010ed3a
Joachim Metz <joachim.metz@gmail.com>
2026-09-14 12:03:51 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libfole/.libs/libfole.1.dylib && test -f ./pyfole/.libs/pyfole.so
+if [ -f "${PWD}/libfole/.libs/libfole.1.dylib" ] && [ -f ./pyfole/.libs/pyfole.so ]
 then
-	install_name_tool -change /usr/local/lib/libfole.1.dylib ${PWD}/libfole/.libs/libfole.1.dylib ./pyfole/.libs/pyfole.so
+    install_name_tool -change /usr/local/lib/libfole.1.dylib "${PWD}/libfole/.libs/libfole.1.dylib" ./pyfole/.libs/pyfole.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
